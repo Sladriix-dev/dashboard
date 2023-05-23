76f76bcc02fa04bae6503737e1dcbaf61e0c07f8
Sladriix <sladriix@gmail.com>
2023-05-23 11:25:52 +0200

    ADD header component

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between px-4 pt-4'>
+        <h2 className='text-black'>Dashboard</h2>
+        <h2 className='text-black'>Bon retour, Kev</h2>
+    </div>
+  )
+}
+
+export default Header
