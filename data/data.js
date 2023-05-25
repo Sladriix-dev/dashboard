f277c53151978ae8b979bfadbb1da65f48f80839
Sladriix <sladriix@gmail.com>
2023-05-25 12:02:30 +0200

    ADD some data for the RecentOrders component

---
+++ data/data.js
@@ -0,0 +1,112 @@
+export const data = [
+  {
+    id: 1,
+    name: {
+      first: "John",
+      last: "Smith",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "PayPal",
+    date: "Il y a 15 minutes",
+  },
+  {
+    id: 2,
+    name: {
+      first: "Chris",
+      last: "Adams",
+    },
+    total: 1195.95,
+    status: "Processing",
+    method: "PayPal",
+    date: "Il y a 23 minutes",
+  },
+  {
+    id: 3,
+    name: {
+      first: "Sarah",
+      last: "Smith",
+    },
+    total: 495.85,
+    status: "Completed",
+    method: "Visa",
+    date: "Il y a 1 heure",
+  },
+  {
+    id: 4,
+    name: {
+      first: "Joseph",
+      last: "Choo",
+    },
+    total: 150.45,
+    status: "Processing",
+    method: "MasterCard",
+    date: "Il y a 1 heure",
+  },
+  {
+    id: 5,
+    name: {
+      first: "Steve",
+      last: "Harding",
+    },
+    total: 175.25,
+    status: "On Hold",
+    method: "PayPal",
+    date: "Il y a 2 heures",
+  },
+  {
+    id: 6,
+    name: {
+      first: "Laura",
+      last: "Croft",
+    },
+    total: 1295.75,
+    status: "Completed",
+    method: "Check",
+    date: "Il y a 3 heures",
+  },
+  {
+    id: 7,
+    name: {
+      first: "Michael",
+      last: "Jones",
+    },
+    total: 89.95,
+    status: "Completed",
+    method: "MasterCard",
+    date: "Il y a 3 heures",
+  },
+  {
+    id: 8,
+    name: {
+      first: "James",
+      last: "Bond",
+    },
+    total: 689.45,
+    status: "Completed",
+    method: "Visa",
+    date: "Il y a 7 heures",
+  },
+  {
+    id: 9,
+    name: {
+      first: "Haley",
+      last: "Whiting",
+    },
+    total: 14.99,
+    status: "Completed",
+    method: "PayPal",
+    date: "Il y a 1 jour",
+  },
+  {
+    id: 10,
+    name: {
+      first: "Tim",
+      last: "Thomas",
+    },
+    total: 218.99,
+    status: "Completed",
+    method: "MasterCard",
+    date: "Il y a 1 jour",
+  },
+];
